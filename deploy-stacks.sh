c3f07f582612c89767c480da3741135058e263e2
Niklas Sombert <niklas@ytvwld.de>
2022-09-21 19:04:08 +0200

    feat(script): Support deploying single stacks

--- deploy-stacks.sh
+++ deploy-stacks.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
-for stackfile in $(ls enabled/);
-do
-    name=$(echo "${stackfile}" | cut -f 1 -d '.')
-    docker stack deploy -c "./enabled/${stackfile}" "${name}"
-done
+deploy() {
+    name=$(echo "$1" | cut -f 2 -d '/' | cut -f 1 -d '.')
+    stackfile=$name.yml
+    echo docker stack deploy -c "./enabled/${stackfile}" "${name}"
+}
+
+if [ -n "$1" ]
+then
+    deploy "$1"
+else
+    for stackfile in enabled/*;
+    do
+        deploy "$stackfile"
+    done
+fi
